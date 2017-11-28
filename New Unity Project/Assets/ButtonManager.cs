7db6a5be57e10e4ee7dae37309619e223bcf26b0
Christian Robles <tricore@nmsu.edu>
2017-11-28 05:45:11 -0700

    Added Unity Project Folder - Contains menu data.

---
+++ New Unity Project/Assets/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void PlayGameBTN(string playGame)
+    {
+        SceneManager.LoadScene(playGame);
+    }
+
+    public void ChooseDeckOneBTN(string deckOne)
+    {
+        SceneManager.LoadScene(deckOne);
+    }
+
+    public void ChooseDeckTwoBTN(string deckTwo)
+    {
+        SceneManager.LoadScene(deckTwo);
+    }
+
+    public void ExitGameBTN()
+    {
+        Application.Quit();
+    }
+}
